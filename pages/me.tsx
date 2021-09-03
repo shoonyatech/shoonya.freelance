f42fd27c9565119cb82e1617a85a1e2a58b5f6b9
Souvik Basu <souvikbasu@gmail.com>
2021-09-03 01:29:07 -0700

    feat(profile): show first user name from graphql

--- pages/me.tsx
+++ pages/me.tsx
@@ -1,10 +1,19 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import React from 'react'
 
+import BaseLayout from '../components/common/BaseLayout'
+import UserNameTitle from '../components/profile/UserNameTitle'
+import ClientSideRendering from '../lib/client-side-rendering'
+
 export default function Me() {
   // const { user, isLoading, error } = useUser()
 
-  return <div>check</div>
+  return (
+    <ClientSideRendering>
+      <UserNameTitle />
+    </ClientSideRendering>
+  )
 }
 
+Me.Layout = BaseLayout
 export const getServerSideProps = withPageAuthRequired()
