e436af5af2328e38c325bc0dd2ecdada5c82dabf
hussamkhatib <hussamkhatib20@gmail.com>
2021-09-13 11:34:26 +0530

    feat(user bio): add mutation,query for user bio

--- components/profile/Bio/index.tsx
+++ components/profile/Bio/index.tsx
@@ -1,28 +1,84 @@
-import TextField from '@material-ui/core/TextField'
-import React, { useState } from 'react'
+/* eslint-disable react/button-has-type */
 
-const Bio = () => 
-{
-  const [edit, setEdit] = useState(false)
-  
-  return (
-  <div className="p-6">
-    {edit ? 
-      <TextField
-      id="outlined-multiline-static"
-      label="Bio"
-      multiline
-      rows={4}
-      variant="outlined"
-      color="secondary"
-      fullWidth
-    /> :
-    <div>
-      18 years of experience in Software development using JavaScript, .Net technologies and Python in various domains like Fintech, Speech and Multilingual Technologies, Network and Platform Security, Telecom and Travel.
-<br />
-Key Skills: React, Angular, Vue, JavaScript, C#
-      </div>
+import { gql, useMutation, useQuery } from '@apollo/client'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import EditIcon from '@material-ui/icons/Edit'
+import React, { ChangeEvent, useEffect, useState } from 'react'
+
+const GET_USER = gql`
+  {
+    user(_id: "613890d00e9d3a2bfc8dd2f7") {
+      bio
+    }
   }
-  </div>
-)}
+`
+const UPDATE_USER = gql`
+  mutation UpdateUserBio($_id: ID!, $bio: String) {
+    updateUserBio(_id: $_id, bio: $bio) {
+      bio
+    }
+  }
+`
+
+const Bio = () => {
+  const [edit, setEdit] = useState<boolean>(false)
+  const { loading, data } = useQuery(GET_USER)
+  const [updateUserBio, { error }] = useMutation(UPDATE_USER)
+  const [bio, setBio] = useState<null | String>('')
+
+  useEffect(() => {
+    if (data) {
+      setBio(data.user.bio)
+    }
+  }, [data])
+  if (loading) return <div>Loading...</div>
+
+  if (error) return <div>Error! ${error.message}</div>
+  const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    setBio(evt.target.value)
+  }
+
+  const updateUser = () => {
+    setEdit(!edit)
+    updateUserBio({
+      variables: { _id: '613890d00e9d3a2bfc8dd2f7', bio },
+      refetchQueries: [{ query: GET_USER }],
+    })
+  }
+
+  return (
+    <div className="p-6">
+      {edit ? (
+          <div className="flex flex-col ">
+            <TextField
+              id="outlined-multiline-static"
+              label="Bio"
+              multiline
+              onChange={handleChange}
+              rows={4}
+              variant="outlined"
+              color="secondary"
+              fullWidth
+            />
+            <div className="self-end m-2">
+              <Button className="mx-1" onClick={() => setEdit(!edit)} variant="contained" color="secondary">
+                Cancel
+              </Button>
+              <Button className="mx-1" onClick={() => updateUser()} variant="contained" color="primary">
+                Save
+              </Button>
+            </div>
+          </div>
+      ) : (
+        <div>
+          <button className="self-start" onClick={() => setEdit(true)}>
+            <EditIcon />
+          </button>
+          <div>{data.user.bio}</div>
+        </div>
+      )}
+    </div>
+  )
+}
 export default Bio
