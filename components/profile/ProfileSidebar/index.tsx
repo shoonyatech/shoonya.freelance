285a7e69d9fc7473d3a6c918f0bbc13550e1d2da
hussamkhatib <hussamkhatib20@gmail.com>
2021-09-19 14:03:05 +0530

    feat(user): add  education section

--- components/profile/ProfileSidebar/index.tsx
+++ components/profile/ProfileSidebar/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Contacts from '../Contacts'
+import Education from '../Education'
 import Skills from '../Skills'
 
 const ProfileSidebar = () => (
   <div className="bg-resume py-4">
     <Contacts />
+    <Education />
     <Skills />
   </div>
 )
