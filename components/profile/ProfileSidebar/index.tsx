c52bea1ddde430188009579169714716491e23b7
hussamkhatib <hussamkhatib20@gmail.com>
2021-09-20 09:35:23 +0530

    feat(user): add page 2 for user profile and developerCommunityInvolvement section

--- components/profile/ProfileSidebar/index.tsx
+++ components/profile/ProfileSidebar/index.tsx
@@ -4,13 +4,17 @@ import Contacts from '../Contacts'
 import Education from '../Education'
 import Skills from '../Skills'
 
-const ProfileSidebar = () => (
+const ProfileSidebar = ({ page }: { page: 1 | 2 }) => (
   <div className="bg-resume py-4">
-    <Contacts />
-    <hr className="h-px border-0 bg-black w-4/5 mx-auto my-6" />
-    <Education />
-    <hr className="h-px border-0 bg-black w-4/5 mx-auto my-6" />
-    <Skills />
+    {page === 1 ? (
+      <>
+        <Contacts />
+        <hr className="h-px border-0 bg-black w-4/5 mx-auto my-6" />
+        <Education />
+        <hr className="h-px border-0 bg-black w-4/5 mx-auto my-6" />
+        <Skills />
+      </>
+    ) : null}
   </div>
 )
 
