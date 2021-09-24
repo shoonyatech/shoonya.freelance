4e0f9b57acef091399a43f3c286e26db61638812
hussamkhatib <hussamkhatib20@gmail.com>
2021-09-24 14:50:32 +0530

    fix(user): rename language to languages

--- components/profile/Languages/index.tsx
+++ components/profile/Languages/index.tsx
@@ -10,14 +10,14 @@ import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react'
 const GET_USER = gql`
   {
     user(_id: "613890d00e9d3a2bfc8dd2f7") {
-      language
+      languages
     }
   }
 `
 const UPDATE_USER = gql`
-  mutation UpdateUserLanguage($_id: ID!, $language: [String]) {
-    updateUserLanguage(_id: $_id, language: $language) {
-      language
+  mutation UpdateUserLanguages($_id: ID!, $languages: [String]) {
+    updateUserLanguages(_id: $_id, languages: $languages) {
+      languages
     }
   }
 `
@@ -33,17 +33,17 @@ const useStyles = makeStyles(() =>
   })
 )
 
-const Languages = () => {
+const Language = () => {
   const [edit, setEdit] = useState<boolean>(false)
   const classes = useStyles()
   const { loading, data } = useQuery(GET_USER)
   const [updateUserLanguage, { error }] = useMutation(UPDATE_USER)
 
-  const [language, setLanguage] = useState<string[]>([])
+  const [languages, setLanguages] = useState<string[]>([])
 
   useEffect(() => {
-    if (data?.user?.language) {
-      setLanguage(data.user.language)
+    if (data?.user?.languages) {
+      setLanguages(data.user.languages)
       setEdit(false)
     } else setEdit(true)
   }, [data])
@@ -53,32 +53,32 @@ const Languages = () => {
   if (error) return <div>Error! ${error.message}</div>
 
   const handleChange = (index: number) => (evt: ChangeEvent<HTMLInputElement>) => {
-    setLanguage([...language.slice(0, index), evt.target.value, ...language.slice(index + 1)])
+    setLanguages([...languages.slice(0, index), evt.target.value, ...languages.slice(index + 1)])
   }
 
   const updateUser = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     await updateUserLanguage({
-      variables: { _id: '613890d00e9d3a2bfc8dd2f7', language },
+      variables: { _id: '613890d00e9d3a2bfc8dd2f7', languages },
       refetchQueries: [{ query: GET_USER }],
     })
     setEdit(!edit)
   }
 
   const cancelUpdateUser = () => {
-    if (data?.user?.language) {
-      setLanguage(data.user.language)
+    if (data?.user?.languages) {
+      setLanguages(data.user.languages)
       setEdit(false)
-    } else setLanguage([])
+    } else setLanguages([])
   }
 
   const addLanguage = () => {
-    setLanguage([...language, ''])
+    setLanguages([...languages, ''])
   }
   return (
     <div className="bg-resume flex flex-col justify-center p-4 md:p-6">
       <div className="flex justify-between pb-3">
-        <h3 className="text-xl md:text-2xl uppercase">Language</h3>
+        <h3 className="text-xl md:text-2xl uppercase">Languages</h3>
         {!edit ? (
           <button onClick={() => setEdit(true)}>
             <EditIcon />
@@ -87,7 +87,7 @@ const Languages = () => {
       </div>
       {edit ? (
         <form className="flex flex-col" onSubmit={updateUser}>
-          {language.map((lang, i): any => (
+          {languages.map((lang, i): any => (
             <div key={i}>
               <TextField
                 name="name"
@@ -119,10 +119,10 @@ const Languages = () => {
           </div>
         </form>
       ) : (
-        <div className="uppercase">{data.user.language.join(' | ')}</div>
+        <div className="uppercase">{data.user.languages.join(' | ')}</div>
       )}
     </div>
   )
 }
 
-export default Languages
+export default Language
