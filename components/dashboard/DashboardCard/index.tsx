ba8045b7b2634acb21c283070cb565af65c1ccfd
hussamkhatib <hussamkhatib20@gmail.com>
2021-09-30 16:01:42 +0530

    feat(dashboard):  create dashboard page , add two cards on it(hire a freelancer, look for projects)

---
+++ components/dashboard/DashboardCard/index.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import Link from 'next/link'
+import React from 'react'
+
+const DashboardCard = ({ children, route }: any) => (
+  <Link href={route}>
+    <a className=" border-2 border-solid border-primary p-4 lg:p-8 my-4 lg:my-8 text-lg lg:text-2xl">{children}</a>
+  </Link>
+)
+
+export default DashboardCard
---
+++ pages/dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import DashboardItems from '../components/dashboard/DashBoardItems'
+
+export default function Dashboard() {
+  return (
+    <div>
+      <DashboardItems />
+    </div>
+  )
+}
