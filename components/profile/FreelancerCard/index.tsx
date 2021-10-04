16b812922b7eb92f7d3a0f5fc366f1271f4830f4
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-10-04 13:31:17 +0530

    feat(freelancer): add freelancer card

--- components/profile/FreelancerCard/index.tsx
+++ components/profile/FreelancerCard/index.tsx
@@ -1,27 +1,23 @@
 import React from 'react'
 
-import Data from './data.json'
-
-function FreelancerCard() {
+function FreelancerCard({ name, skills, experience, rate, currency, image }) {
   return (
-    <div>
-      <div className="flex p-3">
+    <>
+      <div className="flex p-3 mb-3">
         <div>
-          <img className="h-20 w-20 rounded-full" src={Data[0].image} alt="Display Pic" />
+          <img className="h-20 w-20 rounded-full" src={image} alt="Display Pic" />
         </div>
         <div className="ml-3">
-          <p className="text-sm font-bold mb-2">{Data[0].name}</p>
-          <p className="text-sm mb-2">{Data[0].skills.toString()}</p>
-          <p className="text-sm">{Data[0].experience} year exp</p>
+          <p className="text-sm font-bold mb-2">{name}</p>
+          <p className="text-sm mb-2">{skills.toString()}</p>
+          <p className="text-sm">{experience} year exp</p>
         </div>
         <div className="flex absolute right-5">
-          <p className="text-sm">{Data[0].currency}</p>
-          <p className="text-sm" style={{ marginLeft: 2 }}>
-            {Data[0].rate}/hr
-          </p>
+          <p className="text-sm">{currency}</p>
+          <p className="text-sm ml-1">{rate}/hr</p>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- pages/freelancer.tsx
+++ pages/freelancer.tsx
@@ -1,11 +1,36 @@
+import { gql, useQuery } from '@apollo/client'
 import React from 'react'
 
 import FreelancerCard from '../components/profile/FreelancerCard'
 
+const GET_FREELANCER = gql`
+  query Freelancer {
+    freelancer {
+      id
+      name
+      skills
+      experience
+      rate
+      currency
+      image
+    }
+  }
+`
+
 function Freelancer() {
+  const { data } = useQuery(GET_FREELANCER)
   return (
     <div>
-      <FreelancerCard />
+      {data?.freelancer.map((list) => (
+        <FreelancerCard
+          name={list.name}
+          skills={list.skills}
+          experience={list.experience}
+          rate={list.rate}
+          currency={list.currency}
+          image={list.image}
+        />
+      ))}
     </div>
   )
 }
