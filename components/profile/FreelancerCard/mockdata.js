b6dadee1f7af0e9a265b89f53ef691870f1785a9
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-10-13 00:42:35 +0530

    feat(freelancer): add freelancers data

--- components/profile/FreelancerCard/mockdata.js
+++ components/profile/FreelancerCard/mockdata.js
@@ -10,12 +10,21 @@ const freelancers = [
   },
   {
     id: '2',
-    name: 'Souvik Basu',
-    skills: ['React ', 'Vue ', 'Angular'],
-    expInYears: 18,
-    hourlyRate: 500,
+    name: 'Syed Rasheduddin',
+    skills: ['React', 'Javascript'],
+    expInYears: 1,
+    hourlyRate: 400,
     currency: 'INR',
-    image: 'https://pbs.twimg.com/profile_images/1439826328493068288/IyDqiBVT_400x400.jpg',
+    image: 'https://avatars.githubusercontent.com/u/64092741?v=4',
+  },
+  {
+    id: '3',
+    name: 'Hussam Khatib',
+    skills: ['React', 'Graphql'],
+    expInYears: 1,
+    hourlyRate: 400,
+    currency: 'INR',
+    image: 'https://ca.slack-edge.com/TFM2TLDGT-U020ZS8SWR0-fd73a8b50eb3-512',
   },
 ]
 
