25e432cac599892207374b6b747757b5a4283f3a
hussamkhatib <hussamkhatib20@gmail.com>
2021-10-20 14:58:10 +0530

    feat(project): ssr to fetch project details based on project id

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -1,11 +1,47 @@
+import { gql } from '@apollo/client'
+import { GetServerSideProps } from 'next'
 import React from 'react'
 
+import client from '../../apis/apollo.client'
 import ManageProject from '../../components/manageProject/ManageProject'
 
-const Project = () => (
+const GET_PROJECT = gql`
+  query Project($_id: ID!) {
+    project(_id: $_id) {
+      _id
+      owner
+      title
+      skills
+      scope {
+        size
+        duration
+        experience
+      }
+      budget {
+        type
+        currency
+        amount
+      }
+      isPublished
+    }
+  }
+`
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { data } = await client.query({
+    query: GET_PROJECT,
+    variables: { _id: context.query.id },
+  })
+  return {
+    props: {
+      data: data.project,
+    },
+  }
+}
+
+const Project = ({data}) => (
   <div>
-    <ManageProject />
+    <ManageProject data={data}/>
   </div>
 )
-
 export default Project
