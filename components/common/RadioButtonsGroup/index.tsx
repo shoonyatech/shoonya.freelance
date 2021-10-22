03855dd3d63aa9bf967be4a863b02ff93f0d7b4d
hussamkhatib <hussamkhatib20@gmail.com>
2021-10-22 19:53:37 +0530

    feat(projectscope) : add input fields, event handlers, mutation calls

--- components/common/RadioButtonsGroup/index.tsx
+++ components/common/RadioButtonsGroup/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable arrow-body-style */
 import FormControl from '@material-ui/core/FormControl'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import FormLabel from '@material-ui/core/FormLabel'
@@ -6,16 +5,14 @@ import Radio from '@material-ui/core/Radio'
 import RadioGroup from '@material-ui/core/RadioGroup'
 import React from 'react'
 
-const RadioButtonsGroup = ({ formLabel, selectedValue, options, handleChange }) => {
-  return (
-    <FormControl component="fieldset">
-      <FormLabel component="legend">{formLabel}</FormLabel>
-      <RadioGroup aria-label="project budget type" name="type" value={selectedValue} onChange={handleChange}>
-        {options.map((option: string) => (
-          <FormControlLabel key={option} value={option} control={<Radio />} label={option} />
-        ))}
-      </RadioGroup>
-    </FormControl>
-  )
-}
+const RadioButtonsGroup = ({ formLabel, selectedValue, options, handleChange, ariaLabel, name }) => (
+  <FormControl component="fieldset">
+    <FormLabel component="legend">{formLabel}</FormLabel>
+    <RadioGroup aria-label={ariaLabel} name={name} value={selectedValue} onChange={handleChange}>
+      {options.map((option: string) => (
+        <FormControlLabel key={option} value={option} control={<Radio />} label={option} />
+      ))}
+    </RadioGroup>
+  </FormControl>
+)
 export default RadioButtonsGroup
---
+++ components/manageProject/ProjectScope/index.tsx
@@ -0,0 +1,144 @@
+import { gql, useMutation } from '@apollo/client'
+import Button from '@material-ui/core/Button'
+import { createStyles, makeStyles } from '@material-ui/core/styles'
+import EditIcon from '@material-ui/icons/Edit'
+import { useRouter } from 'next/router'
+import React, { useState } from 'react'
+
+import { removeKey } from '../../../lib/utils'
+import RadioButtonsGroup from '../../common/RadioButtonsGroup'
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    btn: {
+      alignSelf: 'flex-end',
+      borderRadius: '999px',
+    },
+    savecancelbtn: {
+      marginRight: '.5rem',
+    },
+  })
+)
+
+const UPDATE_PROJECT_SCOPE = gql`
+  mutation UpdateProjectScope($_id: ID!, $scope: ScopeInput) {
+    updateProjectScope(_id: $_id, scope: $scope) {
+      _id
+      scope {
+        size
+        duration
+        experience
+      }
+    }
+  }
+`
+
+const ProjectScope = ({ data }) => {
+  const router = useRouter()
+  const classes = useStyles()
+
+  const [edit, setEdit] = useState<boolean>(!data)
+  const [projectScope, setProjectScope] = useState(data)
+  const [updatedScope, setUpdatedScope] = useState<string | null>(null)
+
+  const [updateProjectScope, { loading, error }] = useMutation(UPDATE_PROJECT_SCOPE, {
+    onCompleted(val) {
+      const newScope = val.updateProjectScope.scope
+      setProjectScope(newScope)
+      setUpdatedScope(newScope)
+      setEdit(false)
+    },
+  })
+
+  const updateScope = (e) => {
+    e.preventDefault()
+    const filterTypename = removeKey('__typename', projectScope)
+    updateProjectScope({
+      variables: { _id: router.query.id, scope: filterTypename },
+    })
+  }
+
+  const cancel = () => {
+    const revertScope = updatedScope || data
+
+    setProjectScope(revertScope)
+    setEdit(false)
+  }
+
+  const handleChange = (e) =>
+    setProjectScope({
+      ...projectScope,
+      [e.target.name]: e.target.value,
+    })
+
+  if (loading) return <div>Loading...</div>
+  if (error) return <div>Error! {error.message}</div>
+
+  return (
+    <div className="flex flex-col px-6">
+      <div className="flex justify-between pb-3">
+        <h3 className="text-xl md:text-2xl uppercase">scope</h3>
+        {!edit ? (
+          <button type="button" onClick={() => setEdit(true)}>
+            <EditIcon />
+          </button>
+        ) : null}
+      </div>
+
+      {edit ? (
+        <form className="flex flex-col" onSubmit={updateScope}>
+          <RadioButtonsGroup
+            formLabel="Project size"
+            selectedValue={projectScope.size}
+            options={['large', 'medium', 'small']}
+            handleChange={handleChange}
+            name="size"
+            ariaLabel="project size"
+          />
+          <RadioButtonsGroup
+            formLabel="Duration"
+            selectedValue={projectScope.duration}
+            options={['more than 6 months', '3 to 6 months', '1 to 3 months']}
+            handleChange={handleChange}
+            name="duration"
+            ariaLabel="project duration"
+          />
+          <RadioButtonsGroup
+            formLabel="Expertise"
+            selectedValue={projectScope.experience}
+            options={['entry', 'intermediate', 'expert']}
+            handleChange={handleChange}
+            name="experience"
+            ariaLabel="project size"
+          />
+          <div className="self-end pt-2">
+            <div className="self-end pt-2">
+              <Button className={classes.savecancelbtn} type="submit" variant="contained" color="primary">
+                Save
+              </Button>
+              <Button className={classes.savecancelbtn} onClick={() => cancel()} variant="contained" color="secondary">
+                Cancel
+              </Button>
+            </div>
+          </div>
+        </form>
+      ) : (
+        <div>
+          <div className="grid grid-cols-2">
+            <span>Project Size</span> <span>: {projectScope.size}</span>
+          </div>
+          <div className="grid grid-cols-2">
+            <span>Duration</span>
+            <span>: {projectScope.duration}</span>
+          </div>
+          <div className="grid grid-cols-2">
+            <span>Experience</span>
+            <span>: {projectScope.experience}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ProjectScope
