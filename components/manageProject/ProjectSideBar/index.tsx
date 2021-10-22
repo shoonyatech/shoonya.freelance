613f163925f62f91cd62348c6111979abb3da2c2
hussamkhatib <hussamkhatib20@gmail.com>
2021-10-22 19:14:40 +0530

    feat(prjectbudget): add project budget, create common component radiobuttongroup

--- components/manageProject/ProjectSideBar/index.tsx
+++ components/manageProject/ProjectSideBar/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import ProjectBudget from '../ProjectBudget'
 import ProjectTitle from '../ProjectTitle'
 
 const ProjectSideBar = ({ data }) => (
   <div className="bg-resume py-4">
     <ProjectTitle data={data.title} />
+    <hr className="h-px border-0 bg-black w-4/5 mx-auto my-6" />
+    <ProjectBudget data={data.budget} />
   </div>
 )
 
