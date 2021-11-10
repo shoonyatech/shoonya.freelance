43c4b6a41d34a59b6b3ad0cb4bad12bbc3a9b22a
hussamkhatib <hussamkhatib20@gmail.com>
2021-11-10 17:30:56 +0530

    feat(config): add google image provider in config temporarily

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  env: {
-    WALLORA_BACKEND_SUBMIT_FEEDBACK: 'https://wt-5c9a92f46a5c0d255deef1b49b7375ca-0.run.webtask.io/wallora-feedback',
-    WALLORA_BACKEND_TOTAL_PLANNED_AMOUNT_WORKITEMWISE:
-      'https://wallora.com/api/stats/total-planned-amount-workitemwise',
-    WALLORA_BACKEND_TOTAL_PLANNED_EXPENSES_MONTHWISE: 'https://wallora.com/api/stats/total-planned-amount-monthwise',
-  },
   images: {
-    domains: ['s.gravatar.com'],
+    domains: ['s.gravatar.com', 'lh3.googleusercontent.com'],
   },
 }
