e18ca88e057330ae8a75f0f4cc13bb9313d3d260
hussamkhatib <hussamkhatib20@gmail.com>
2021-11-15 22:24:11 +0530

    feat(proposal): add project in right side of proposals

--- src/components/proposals/ProposalCard/index.tsx
+++ src/components/proposals/ProposalCard/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-const ProposalCard = ({ coverLetter, budget }) => (
-  <div className="flex flex-col border-solid border-2 border-primary m-4 p-4">
-    <p>{coverLetter}</p>
-    <p>Budget : {budget}</p>
-  </div>
-)
+const ProposalCard = ({ data, updateActiveProjectId }) => {
+  const { projectId, coverLetter, budget } = data
+  return (
+    <button
+      type="button"
+      onClick={() => updateActiveProjectId(projectId)}
+      className="flex flex-col border-solid border-2 border-primary m-4 p-4"
+    >
+      <p>{coverLetter}</p>
+      <p>Budget : {budget}</p>
+    </button>
+  )
+}
 
 export default ProposalCard
