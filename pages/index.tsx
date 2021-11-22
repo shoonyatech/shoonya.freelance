b606a087523a148eb7bbc2b8e8979aee407421b7
hussamkhatib <hussamkhatib20@gmail.com>
2021-11-22 11:37:03 +0530

    feat(home): remove wallora content from home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import { useUser } from '@auth0/nextjs-auth0'
 import React from 'react'
 
 import Loader from '../src/components/common/Loader'
-import MainContent from '../src/components/home/MainContent'
 
 export default function Home() {
   // user
@@ -13,9 +12,5 @@ export default function Home() {
         <Loader open={isLoading} error={error} />
       </div>
     )
-  return (
-    <div>
-      <MainContent />
-    </div>
-  )
+  return <div />
 }
