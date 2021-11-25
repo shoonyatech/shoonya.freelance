16b3564a485213ddf58d6ea7a715e823eac6d565
hussamkhatib <hussamkhatib20@gmail.com>
2021-11-25 18:52:07 +0530

    fix(general): minor bugs and refactoring

--- src/components/proposals/Proposals/index.tsx
+++ src/components/proposals/Proposals/index.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 import { isArrayEmpty } from '../../../lib/utils'
 import MasterDetailsLayout from '../../common/MasterDetailsLayout'
+import FreelancerList from '../../freelancer/FreelancerList'
 import Profile from '../../profile/Profile'
-import ProposalList from '../ProposalList'
 
 const Proposals = ({ data, freelancers, updateActiveProject, activeFreelancer }) => {
   if (isArrayEmpty(freelancers)) return <p>No proposals</p>
 
   return (
     <MasterDetailsLayout>
-      <ProposalList data={data} freelancers={freelancers} updateActiveProject={updateActiveProject} />
+      <FreelancerList data={data} freelancers={freelancers} updateActiveProject={updateActiveProject} />
       <Profile data={activeFreelancer} isReadOnly />
     </MasterDetailsLayout>
   )
