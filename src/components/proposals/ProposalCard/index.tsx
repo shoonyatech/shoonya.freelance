9aaf2515ecbbd25a3968b85d46c13941ee1a299d
hussamkhatib <hussamkhatib20@gmail.com>
2021-11-26 12:47:11 +0530

    remove proposals component from my-proposals

--- src/components/proposals/ProposalCard/index.tsx
+++ src/components/proposals/ProposalCard/index.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const ProposalCard = ({ data }) => {
   const { coverLetter } = data
   return (
-    <button type="button" className="flex flex-col border-solid border-2 border-primary m-4 p-4">
+    <div className="flex flex-col p-4">
       <p>{coverLetter}</p>
-    </button>
+    </div>
   )
 }
 
