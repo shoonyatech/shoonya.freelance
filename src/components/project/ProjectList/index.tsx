cd77b4d001b0456818c9588baf3fbd46f3ee9f83
hussamkhatib <hussamkhatib20@gmail.com>
2021-12-02 17:10:40 +0530

    feat(my-proposals): add active proposal's project on right panel

--- src/components/project/ProjectList/index.tsx
+++ src/components/project/ProjectList/index.tsx
@@ -1,15 +1,15 @@
 /* eslint-disable no-underscore-dangle */
 import React from 'react'
 
-const ProjectList = ({ data, updateActiveProposalId, activeProjectId }: any) => (
+const ProjectList = ({ data, updateActiveProject, activeProjectId }: any) => (
   <div className="flex flex-col overflow-y-auto max-h-screen">
     {data.map((proposal: any) => (
       <button
         key={proposal._id}
-        onClick={() => updateActiveProposalId(proposal._id)}
+        onClick={() => updateActiveProject(proposal.projectId)}
         type="button"
         className={`flex flex-col border-2 border-solid m-4 p-4 ${
-          proposal._id === activeProjectId ? 'border-4 ' : ' border-2'
+          proposal.projectId === activeProjectId ? 'border-4 ' : ' border-2'
         }`}
       >
         <p className="text-xl font-bold">{proposal?.projectTitle}</p>
