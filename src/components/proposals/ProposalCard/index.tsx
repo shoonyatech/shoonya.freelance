400554a1d52a634d75988da93131246ddf6d235e
hussamkhatib <hussamkhatib20@gmail.com>
2021-12-04 11:22:53 +0530

    fix(proposal): add new proposal gql

--- src/components/proposals/ProposalCard/index.tsx
+++ src/components/proposals/ProposalCard/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const ProposalCard = ({ data }) => {
-  const { coverLetter, propossedRate } = data
+  const { coverLetter, proposedRate } = data
 
   return (
     <div className="flex flex-col p-4">
       <p className="text-3xl py-1">Cover Letter</p>
       <p>{coverLetter}</p>
       <p className="text-xl py-1">
-        Proposed Rate : <span className="font-semibold">{propossedRate}</span>
+        Proposed Rate : <span className="font-semibold">{proposedRate}</span>
       </p>
     </div>
   )
