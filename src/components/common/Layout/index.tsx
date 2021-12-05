4cfc28e16e456ced95b4f90b5e7a0ff8f9e35296
hussamkhatib <hussamkhatib20@gmail.com>
2021-12-05 11:16:29 +0530

    feat(head): add favicons

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -20,15 +20,14 @@ function Layout({ children }: any): React.ReactElement {
     <div className="container min-w-full grid-cols-1 grid-rows-3 overflow-x-hidden">
       <Head>
         <title>Shoonya</title>
-        <link rel="icon" href="assets/favicon.ico" />
-        <link rel="manifest" href="assets/site.webmanifest" />
-        <link rel="mask-icon" href="assets/safari-pinned-tab.svg" color="#5bbad5" />
-        <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png" />
-        <meta name="apple-mobile-web-app-title" content="wallora" />
-        <meta name="application-name" content="wallora" />
-        <meta name="msapplication-TileColor" content="#da532c" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="application-name" content="shoonya freelance" />
+        <meta name="apple-mobile-web-app-title" content="shoonya freelance" />
+
         <meta name="theme-color" content="#ffffff" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700%26display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
