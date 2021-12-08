61dee134547c7575c3c618acc76299a7b997506b
hussamkhatib <hussamkhatib20@gmail.com>
2021-12-08 22:10:25 +0530

    fix(projects): show budget level $,1073,1073$ based on amount/ remove the old random generator

---
+++ src/lib/currency.ts
@@ -0,0 +1,13 @@
+
+// todo : makeit more generalized
+
+export const getBudgetLevel = (currencySymbol:string,maxLevel:number,rate:number,avgRate:number) => {
+	if(rate > avgRate)
+  return currencySymbol.repeat(maxLevel)
+  if(rate < (avgRate * (2/5)))
+  return currencySymbol
+  else 
+  return currencySymbol.repeat(maxLevel-1)
+}
+
+console.log(getBudgetLevel("$",3,10,50))
