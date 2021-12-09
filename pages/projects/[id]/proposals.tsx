7957bf82379a02611212f326ef79344aa6b9e309
hussamkhatib <hussamkhatib20@gmail.com>
2021-12-09 16:04:26 +0530

    refactor(proposals): remove unwanted code

--- pages/projects/[id]/proposals.tsx
+++ pages/projects/[id]/proposals.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 import { useQuery } from '@apollo/client'
-import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0'
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import { GetServerSideProps } from 'next'
 import React from 'react'
 
@@ -8,7 +8,6 @@ import GetApolloClient from '../../../apis/apollo.client'
 import Loader from '../../../src/components/common/Loader'
 import Proposals from '../../../src/components/proposals/Proposals'
 import { GET_PROPOSALS_BY_PROJECT, GET_USER } from '../../../src/gql/user'
-import { getUserId } from '../../../src/lib/user-helper'
 import { isArrayEmpty } from '../../../src/lib/utils'
 
 const client = GetApolloClient(process.env.GRAPHQL_SERVER)
@@ -44,22 +43,13 @@ export default function MyProposal({ data, isProposalsEmpty }) {
 
 export const getServerSideProps: GetServerSideProps = withPageAuthRequired({
   async getServerSideProps(context) {
-    const session = getSession(context.req, context.res)
-    const userId = getUserId(session?.user.sub)
-
     const { data } = await client.query({
       query: GET_PROPOSALS_BY_PROJECT,
       variables: { _id: context.query.id },
       errorPolicy: 'ignore',
     })
 
-    const { getProposalsByProject, getProjectOwner } = data
-    // temporary show 404 if user is not the owner of this project
-    if (getProjectOwner.owner !== userId) {
-      return {
-        notFound: true,
-      }
-    }
+    const { getProposalsByProject } = data
 
     if (isArrayEmpty(getProposalsByProject))
       return {
