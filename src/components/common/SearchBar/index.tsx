39e529181b9c6fa04fd9309c8fe01f2d8047489e
hussamkhatib <hussamkhatib20@gmail.com>
2021-12-15 22:09:37 +0530

    feat(iconpickor): add skilliconpickor as a seperate component

--- src/components/common/SearchBar/index.tsx
+++ src/components/common/SearchBar/index.tsx
@@ -1,17 +1,23 @@
+import { createStyles, makeStyles } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
 import React from 'react'
 
-const SearchBar = ({ list, label, handleFilter }) => {
+const useStyles = makeStyles(() =>
+  createStyles({
+    search: {
+      margin: '0 0.5em',
+    },
+  })
+)
+
+const SearchBar = ({ list, label, handleFilter }: { list: any; label: string; handleFilter: any }) => {
+  const classes = useStyles()
   const handleOnKeyUp = (e) => {
     const filter = list.filter((str) => str.toLowerCase().indexOf(e.target.value.toLowerCase()) !== -1)
     handleFilter(filter)
   }
 
-  return (
-    <div className="px-2">
-      <TextField id="standard-basic" label={label} onKeyUp={handleOnKeyUp} fullWidth />
-    </div>
-  )
+  return <TextField id="standard-basic" label={label} onKeyUp={handleOnKeyUp} fullWidth className={classes.search} />
 }
 
 export default SearchBar
---
+++ src/components/common/SkillIconPickor/SkillIconPickor.stories.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+
+import SkillIconPickor from '.'
+
+export default {
+  component: SkillIconPickor,
+  title: 'SkillIconPickor',
+} as Meta
+
+const Template: Story = (args) => <SkillIconPickor {...args} />
+export const Default = Template.bind({})
+
+Default.args = {
+  displayIcon: true,
+  isActive: true,
+  closeIconPickor: undefined,
+  selectedIcons: [],
+  handleSkillChange: undefined,
+}
+
+export const DisplayIcons = Template.bind({})
+DisplayIcons.args = {
+  ...Default.args,
+}
+
+export const HideIcons = Template.bind({})
+HideIcons.args = {
+  ...Default.args,
+  displayIcon: false,
+}
