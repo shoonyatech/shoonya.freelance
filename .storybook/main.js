248960848e14bd082bf36e700a6a11a7f7fb128a
hussamkhatib <hussamkhatib20@gmail.com>
2021-12-15 16:32:02 +0530

    build(sb): upgrade storybook to latest and fix breaking change that came with it

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,5 +3,13 @@ module.exports = {
     "../src/components/**/*.stories.mdx",
     "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"]
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", {
+    name: "@storybook/addon-postcss",
+    options: {
+      postcssLoaderOptions: {
+        implementation: require("postcss"),
+      },
+    },
+  }
+  ]
 };
