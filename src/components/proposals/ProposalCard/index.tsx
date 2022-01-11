77dd405a514c0d7d6f963da64c38cfc9335da9c4
hussamkhatib <hussamkhatib20@gmail.com>
2022-01-11 22:03:27 +0530

    feat(page): add proposals/ page and render the corresponding proposal by the id

--- src/components/proposals/ProposalCard/index.tsx
+++ src/components/proposals/ProposalCard/index.tsx
@@ -4,14 +4,12 @@ const ProposalCard = ({ data }) => {
   const { coverLetter, proposedRate, currency } = data
   return (
     <div className="flex flex-col p-4">
-      <p className="text-3xl py-1">Cover Letter</p>
-      <p>{coverLetter}</p>
-      <p className="text-xl py-1">
-        Proposed Rate :{' '}
-        <span className="font-semibold">
-          {currency}
-          {proposedRate}
-        </span>
+      <p className="text-3xl py-1">Covering Letter</p>
+      <p className="mb-4 mt-2">{coverLetter}</p>
+      <p className="text-xl py-1">Proposed Rate</p>
+      <p className="font-semibold">
+        {currency}
+        {proposedRate}
       </p>
     </div>
   )
