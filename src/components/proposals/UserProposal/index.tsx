39384571d65622a7ab28fe4c0ad3641f48567928
hussamkhatib <hussamkhatib20@gmail.com>
2022-01-15 13:11:28 +0530

    feat(proposal): add UserProposal component where user can view and edit his proposal, note: delete proposal functionality is not added in this commit

---
+++ src/components/proposals/UserProposal/index.tsx
@@ -0,0 +1,70 @@
+/* eslint-disable no-underscore-dangle */
+import { useMutation } from '@apollo/client'
+import Button from '@material-ui/core/Button'
+import React, { useState } from 'react'
+
+import { UPDATE_PROPOSAL } from '../../../gql/proposal'
+import Loader from '../../common/Loader'
+import ProjectProposal from '../../project/apply/ProjectProposal'
+import ProposalCard from '../ProposalCard'
+
+const UserProposal = ({ initialData }) => {
+  const [data, setData] = useState(initialData)
+  const [edit, setEdit] = useState(false)
+
+  const toggleEdit = () => setEdit((state) => !state)
+  const cancelProposal = () => {
+    toggleEdit()
+    setData(initialData)
+  }
+
+  const handleChange = (key, newValue) => {
+    setData({
+      ...data,
+      [key]: newValue,
+    })
+  }
+  const [updateUserProposal, { loading, error }] = useMutation(UPDATE_PROPOSAL, {
+    variables: {
+      id: initialData._id,
+      coverLetter: data.coverLetter,
+      proposedRate: data.proposedRate,
+    },
+    onCompleted({ updateProposal }) {
+      setData(updateProposal)
+      toggleEdit()
+    },
+  })
+
+  if (loading) return <Loader open={loading} error={error} />
+
+  return (
+    <div className="flex flex-col gap-y-4 my-4 px-6 py-2">
+      {edit ? (
+        <>
+          <ProjectProposal
+            submitProposal={updateUserProposal}
+            currency={initialData.currency}
+            cancelProposal={cancelProposal}
+            data={data}
+            handleChange={handleChange}
+          />
+        </>
+      ) : (
+        <>
+          <div className="flex gap-x-4">
+            <Button onClick={() => toggleEdit()} variant="contained" color="primary">
+              Edit
+            </Button>
+            <Button variant="contained" color="primary">
+              Delete
+            </Button>
+          </div>
+          <ProposalCard data={data} />
+        </>
+      )}
+    </div>
+  )
+}
+
+export default UserProposal
