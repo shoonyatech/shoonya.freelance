c8c0a47a50224c08b61fc0c40b20d736f78c287c
hussamkhatib <hussamkhatib20@gmail.com>
2022-01-15 13:11:28 +0530

    feat(proposal): add UserProposal component where user can view and edit his proposal, note: delete proposal functionality is not added in this commit

--- pages/proposals/[id].tsx
+++ pages/proposals/[id].tsx
@@ -1,29 +1,24 @@
-// import { getSession } from '@auth0/nextjs-auth0'
 import { GetServerSideProps } from 'next'
 import React from 'react'
 
 import GetApolloClient from '../../apis/apollo.client'
-import ProposalCard from '../../src/components/proposals/ProposalCard'
+import UserProposal from '../../src/components/proposals/UserProposal'
 import { GET_PROPOSAL_BY_ID } from '../../src/gql/proposal'
-// import { getUserId } from '../../src/lib/user-helper'
 
 const client = GetApolloClient(process.env.GRAPHQL_SERVER)
 
 // eslint-disable-next-line arrow-body-style
-const Proposal = ({ initialData }) => {
+const ProposalPage = ({ initialData }) => {
   return (
     <div style={{ marginLeft: '57px' }}>
-      <ProposalCard data={initialData} />
+      <UserProposal initialData={initialData} />
     </div>
   )
 }
 
-export default Proposal
+export default ProposalPage
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  // const session = getSession(context.req, context.res)
-  // const userId = getUserId(session?.user.sub)
-
   const { data } = await client.query({
     query: GET_PROPOSAL_BY_ID,
     variables: { _id: context.query.id },
