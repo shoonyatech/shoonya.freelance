e61a210b8924a2df45c73ec18e03fb652b47b742
hussamkhatib <hussamkhatib20@gmail.com>
2022-01-18 19:25:10 +0530

    feat(proposals): add action btn's for projects/id/proposals page/(dummy btn's)

--- src/components/common/MasterDetailsLayout/index.tsx
+++ src/components/common/MasterDetailsLayout/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const MasterDetailsLayout = ({ children }) => (
-  <div className=" lg:grid lg:grid-cols-masterDetailsLayout gap-4">{children}</div>
+  <div className=" lg:grid lg:grid-cols-masterDetailsLayout gap-2">{children}</div>
 )
 
 export default MasterDetailsLayout
--- src/components/proposals/Proposals/index.tsx
+++ src/components/proposals/Proposals/index.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-underscore-dangle */
+import Button from '@material-ui/core/Button'
 import React, { useState } from 'react'
 
 import { isArrayEmpty } from '../../../lib/utils'
@@ -14,7 +15,21 @@ const Proposals = ({ initialData }) => {
   return (
     <MasterDetailsLayout>
       <FreelancerList data={initialData} updateActiveProject={updateActiveProject} />
-      <div>
+      <div className="border-2 border-solid m-4 px-4 py-3">
+        <div className="flex gap-x-4">
+          <Button variant="contained" color="primary">
+            Shortlist
+          </Button>
+          <Button variant="contained" color="primary">
+            Reject
+          </Button>
+          <Button variant="contained" color="primary">
+            Hire
+          </Button>
+          <Button variant="contained" color="primary">
+            Details
+          </Button>
+        </div>
         <ProposalCard data={initialData[activeIndex]} />
       </div>
     </MasterDetailsLayout>
