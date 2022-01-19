dcb11473daab52d368550c0d34207457892bb347
hussamkhatib <hussamkhatib20@gmail.com>
2022-01-19 11:29:15 +0530

    refactor(proposals): remove /hr from freelancer card and add currency type to it instead,rename details to View profile

--- src/components/freelancer/FreelancerList/index.tsx
+++ src/components/freelancer/FreelancerList/index.tsx
@@ -4,18 +4,21 @@ import React from 'react'
 
 import FreelancerStrip from '../FreelancerStrip'
 
-const FreelancerList = ({ data, updateActiveProject }) => (
-  <div className="flex flex-col">
-    {data?.map((freelancer, i) => (
-      <FreelancerStrip
-        rate={freelancer.proposedRate}
-        key={freelancer._id}
-        freelancer={freelancer.proposser}
-        updateFreelancerIndex={updateActiveProject}
-        index={i}
-      />
-    ))}
-  </div>
-)
-
+const FreelancerList = ({ data, updateActiveProject }) => {
+  const { currency } = data[0]
+  return (
+    <div className="flex flex-col">
+      {data?.map((freelancer, i) => (
+        <FreelancerStrip
+          rate={freelancer.proposedRate}
+          key={freelancer._id}
+          currency={currency}
+          freelancer={freelancer.proposser}
+          updateFreelancerIndex={updateActiveProject}
+          index={i}
+        />
+      ))}
+    </div>
+  )
+}
 export default FreelancerList
