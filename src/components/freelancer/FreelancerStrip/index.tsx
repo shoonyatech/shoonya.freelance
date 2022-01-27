94e6c07d5077b1c7b8aabce315e0ac7b91ce8084
hussamkhatib <hussamkhatib20@gmail.com>
2022-01-27 21:57:30 +0530

    feat(users): display message btn on a freelancer profile

--- src/components/freelancer/FreelancerStrip/index.tsx
+++ src/components/freelancer/FreelancerStrip/index.tsx
@@ -8,7 +8,10 @@ const FreelancerStrip = ({ freelancer, rate, updateFreelancerIndex, currency, in
     <button type="button" onClick={() => updateFreelancerIndex(index)} className="flex p-3 m-4 border-2 border-solid">
       <div className="flex flex-col items-center">
         {picture ? <Image height={68} width={68} src={picture} className="rounded-full" /> : null}
-        <p>{currency}{rate}</p>
+        <p>
+          {currency}
+          {rate}
+        </p>
       </div>
       <div className="flex flex-col pl-4">
         <div className="flex-1">
