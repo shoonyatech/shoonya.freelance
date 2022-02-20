72575dbe465052b11a90cd9cd149cf2626cfd9dc
hussamkhatib <hussamkhatib20@gmail.com>
2022-02-20 13:06:59 +0530

    feat(sidebar): removed mui components and recreated it using tailwind, header missing

--- src/components/common/SideBar/index.tsx
+++ src/components/common/SideBar/index.tsx
@@ -1,10 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { useUser } from '@auth0/nextjs-auth0'
-import { Divider, IconButton, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
-import Drawer from '@material-ui/core/Drawer'
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
-import MenuIcon from '@material-ui/icons/Menu'
-import clsx from 'clsx'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -13,142 +8,82 @@ import { FaBriefcase } from 'react-icons/fa'
 import { FiLogOut } from 'react-icons/fi'
 import { IoBarChart } from 'react-icons/io5'
 import { RiSettings3Fill } from 'react-icons/ri'
-import { useToggle } from 'react-use'
 
-const drawerWidth = 240
+const primaryTabs = [
+  {
+    name: 'Dashboard',
+    href: '/dashboard',
+    icon: <IoBarChart color="#E8D4B2" size="1.5em" />,
+  },
+  {
+    name: 'Projects',
+    href: '/projects',
+    icon: <FaBriefcase color="#E8D4B2" size="1.5em" />,
+  },
+  {
+    name: 'Freelancers',
+    href: '/freelancers',
+    icon: <BsPersonBoundingBox color="E8D4B2" size="1.5em" />,
+  },
+]
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    drawer: {
-      width: drawerWidth,
-      flexShrink: 0,
-      whiteSpace: 'nowrap',
-      '@media (max-width:639px)': { display: 'none' },
-    },
-    drawerOpen: {
-      backgroundColor: theme.palette.primary.main,
-      width: drawerWidth,
-      transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    },
-    drawerClose: {
-      backgroundColor: theme.palette.primary.main,
-      transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-      overflowX: 'hidden',
-      width: theme.spacing(7) + 1,
-      [theme.breakpoints.up('sm')]: {
-        width: theme.spacing(7) + 1,
-      },
-    },
-    list: {
-      color: '#E8D4B2',
-    },
-  })
-)
+const secondaryTabs = [
+  {
+    name: 'Settings',
+    href: '/settings',
+    icon: <RiSettings3Fill color="#E8D4B2" size="1.5em" />,
+  },
+  {
+    name: 'Sign out',
+    href: '/api/auth/logout',
+    icon: <FiLogOut color="#E8D4B2" size="1.5em" />,
+  },
+]
 
 export default function MiniDrawer() {
   const { user } = useUser()
-  const classes = useStyles()
-  const [open, toggleIsOn] = useToggle(false)
 
   return (
-    <div className="">
-      <Drawer
-        variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-        })}
-        classes={{
-          paper: clsx({
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-          }),
-        }}
-      >
-        <div className="pl-0 pr-0 flex items-center">
-          <IconButton onClick={toggleIsOn} color="secondary" aria-label="open drawer" className="w-14 h-14">
-            <MenuIcon color="secondary" />
-          </IconButton>
-          <Link href="/" passHref>
-            <a>
-              <h1 className="font-bold text-white text-xl md:text-2xl">Shoonya</h1>
-            </a>
-          </Link>
-        </div>
+    <header className="bg-primary text-secondary w-16 h-screen">
+      <nav className="h-full">
+        <ul className="py-2 h-full flex flex-col justify-between">
+          <div>
+            {primaryTabs.map((tab) => (
+              <li className="p-2">
+                <Link href={tab.href}>
+                  <a className="flex flex-col items-center">
+                    {tab.icon}
+                    <span className="text-xs"> {tab.name}</span>{' '}
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </div>
 
-        <Divider />
-
-        <List>
-          <Link href="/dashboard" passHref>
-            <ListItem button className={classes.list} key="dashboard">
-              <ListItemIcon>
-                <IoBarChart color="#E8D4B2" size="1.5em" />
-              </ListItemIcon>
-              <ListItemText primary="Dashboard" />
-            </ListItem>
-          </Link>
-        </List>
-        <List>
-          <Link href="/projects" passHref>
-            <ListItem button className={classes.list} key="projects">
-              <ListItemIcon>
-                <FaBriefcase color="#E8D4B2" size="1.5em" />
-              </ListItemIcon>
-
-              <ListItemText primary="Projects" />
-            </ListItem>
-          </Link>
-        </List>
-        <List>
-          <Link href="/freelancers" passHref>
-            <ListItem button className={classes.list} key="freelancers">
-              <ListItemIcon>
-                <BsPersonBoundingBox color="E8D4B2" size="1.5em" />
-              </ListItemIcon>
-
-              <ListItemText primary="Freelancers" />
-            </ListItem>
-          </Link>
-        </List>
-        <List style={{ marginTop: `auto` }}>
-          <Divider />
-          <List>
-            <Link href="/me" passHref>
-              <ListItem button className={classes.list} key="profile">
-                <ListItemIcon>
+          <div>
+            <li className="p-2">
+              <Link href="/me">
+                <a className="flex flex-col items-center">
                   {user?.picture ? (
                     <Image className="rounded-full" src={user?.picture} alt="avatar" height="24" width="24" />
                   ) : null}
-                </ListItemIcon>
-                <ListItemText primary="Profile" />
-              </ListItem>
-            </Link>
-          </List>
-
-          <Link href="/settings" passHref>
-            <ListItem button className={classes.list} key="Settings">
-              <ListItemIcon>
-                <RiSettings3Fill color="#E8D4B2" size="1.5em" />
-              </ListItemIcon>
-              <ListItemText primary="Settings" />
-            </ListItem>
-          </Link>
-          <Link href="/api/auth/logout" passHref>
-            <ListItem button className={classes.list} key="Sign out">
-              <ListItemIcon>
-                <FiLogOut color="#E8D4B2" size="1.5em" />
-              </ListItemIcon>
-              <ListItemText primary="Sign Out" />
-            </ListItem>
-          </Link>
-        </List>
-      </Drawer>
-    </div>
+                  <span className="text-xs"> Profile</span>
+                </a>
+              </Link>
+            </li>
+            {secondaryTabs.map((tab) => (
+              <li className="p-2">
+                <Link href={tab.href}>
+                  <a className="flex flex-col items-center">
+                    {tab.icon}
+                    <span className="text-xs"> {tab.name}</span>
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </div>
+        </ul>
+      </nav>
+    </header>
   )
 }
