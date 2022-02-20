72e5aa9cd7b03c39dae9343c1cc5b1ace5c42777
hussamkhatib <hussamkhatib20@gmail.com>
2022-02-20 14:19:52 +0530

    feat(sidebar): add aria-current for active page for ally, background and text change for active tab

--- src/components/common/SideBar/index.tsx
+++ src/components/common/SideBar/index.tsx
@@ -2,6 +2,7 @@
 import { useUser } from '@auth0/nextjs-auth0'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React from 'react'
 import { BsPersonBoundingBox } from 'react-icons/bs'
 import { FaBriefcase } from 'react-icons/fa'
@@ -13,17 +14,17 @@ const primaryTabs = [
   {
     name: 'Dashboard',
     href: '/dashboard',
-    icon: <IoBarChart color="#E8D4B2" size="1.5em" />,
+    icon: IoBarChart,
   },
   {
     name: 'Projects',
     href: '/projects',
-    icon: <FaBriefcase color="#E8D4B2" size="1.5em" />,
+    icon: FaBriefcase,
   },
   {
     name: 'Freelancers',
     href: '/freelancers',
-    icon: <BsPersonBoundingBox color="E8D4B2" size="1.5em" />,
+    icon: BsPersonBoundingBox,
   },
 ]
 
@@ -31,17 +32,18 @@ const secondaryTabs = [
   {
     name: 'Settings',
     href: '/settings',
-    icon: <RiSettings3Fill color="#E8D4B2" size="1.5em" />,
+    icon: RiSettings3Fill,
   },
   {
     name: 'Sign out',
     href: '/api/auth/logout',
-    icon: <FiLogOut color="#E8D4B2" size="1.5em" />,
+    icon: FiLogOut,
   },
 ]
 
 export default function MiniDrawer() {
   const { user } = useUser()
+  const router = useRouter();
 
   return (
     <header className="hidden sm:block bg-primary text-secondary w-16 h-screen">
@@ -51,38 +53,46 @@ export default function MiniDrawer() {
       <nav className='h-full'>
         <ul className="py-2 h-full flex flex-col justify-between">
           <div className='mt-20'>
-            {primaryTabs.map((tab) => (
-              <li className="p-2">
-                <Link href={tab.href}>
-                  <a className="flex flex-col items-center">
-                    {tab.icon}
-                    <span className="text-xs mt-1"> {tab.name}</span>{' '}
-                  </a>
-                </Link>
-              </li>
-            ))}
+            {primaryTabs.map((tab) => {
+              const isCurrent = router.asPath === tab.href;
+              return (
+                <li className={`p-2 ${isCurrent && "bg-secondary text-primary"}`}>
+                  <Link href={tab.href}>
+                    <a className="flex flex-col items-center" aria-current={isCurrent ? "page" : undefined}>
+                      <tab.icon size="1.25em" color={isCurrent ? "#282828" : "#E8D4B2"} />
+                      <span className="text-xs mt-1">{tab.name}</span>{' '}
+                    </a>
+                  </Link>
+                </li>
+              )
+            })}
           </div>
           <div>
-            <li className="p-2">
+            <li className={`p-2 ${router.asPath === './me' && "bg-secondary text-primary"}`}>
               <Link href="/me">
                 <a className="flex flex-col items-center">
                   {user?.picture ? (
-                    <Image className="rounded-full" src={user?.picture} alt="avatar" height="24" width="24" />
+                    <Image className="rounded-full" src={user?.picture} alt="avatar" height="20" width="20" />
                   ) : null}
                   <span className="text-xs"> Profile</span>
                 </a>
               </Link>
             </li>
-            {secondaryTabs.map((tab) => (
-              <li className="p-2">
-                <Link href={tab.href}>
-                  <a className="flex flex-col items-center">
-                    {tab.icon}
-                    <span className="text-xs"> {tab.name}</span>
-                  </a>
-                </Link>
-              </li>
-            ))}
+
+            {secondaryTabs.map((tab) => {
+              const isCurrent = router.asPath === tab.href;
+
+              return (
+                <li className={`p-2 ${isCurrent && "bg-secondary text-primary"}`}>
+                  <Link href={tab.href}>
+                    <a className="flex flex-col items-center">
+                      <tab.icon size="1.25em" color={isCurrent ? "#282828" : "#E8D4B2"} />
+                      <span className="text-xs"> {tab.name}</span>
+                    </a>
+                  </Link>
+                </li>
+              )
+            })}
           </div>
         </ul>
       </nav>
