b02cfea83998831e7ac3b6ed97db4ef1e2270b8f
hussamkhatib <hussamkhatib20@gmail.com>
2022-05-12 15:35:21 +0530

    feat(tailwind): bump v2 to v3, fixes #56

--- src/components/common/Slider/index.tsx
+++ src/components/common/Slider/index.tsx
@@ -5,7 +5,7 @@ const SliderContainer = ({ children, openOrCloseSlider }) => (
     className={`${openOrCloseSlider}
                 ? "translate-x-0 ease-out"
                 : "translate-x-full ease-in"
-            } fixed right-0 top-0 max-w-sm w-full h-full p-4 transition duration-300 transform overflow-y-auto bg-white border-l-2 border-gray-300`}
+            } fixed right-0 top-0 max-w-sm w-full h-full p-4 transition duration-300 overflow-y-auto bg-white border-l-2 border-gray-300`}
   >
     {children}
   </div>
