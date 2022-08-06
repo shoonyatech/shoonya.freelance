8f5d7fe69fcc5721c9dc8e1208d6d78e664d7634
hussamkhatib <hussamkhatib@gmail.com>
2022-08-06 22:55:58 +0530

    tools(eslint): modify extends array order

--- src/components/projects/Projects/index.tsx
+++ src/components/projects/Projects/index.tsx
@@ -4,13 +4,11 @@ import MasterDetailsLayout from '../../common/MasterDetailsLayout'
 import ManageProject from '../../manageProject/ManageProject'
 import ProjectList from '../ProjectList'
 
-const Projects = ({ data, updateActiveProjectId, activeProject }) => {
-  return (
-    <MasterDetailsLayout>
-      <ProjectList activeProjectId={activeProject._id} updateActiveProjectId={updateActiveProjectId} data={data} />
-      <ManageProject data={activeProject} isReadOnly />
-    </MasterDetailsLayout>
-  )
-}
+const Projects = ({ data, updateActiveProjectId, activeProject }) => (
+  <MasterDetailsLayout>
+    <ProjectList activeProjectId={activeProject._id} updateActiveProjectId={updateActiveProjectId} data={data} />
+    <ManageProject data={activeProject} isReadOnly />
+  </MasterDetailsLayout>
+)
 
 export default Projects
